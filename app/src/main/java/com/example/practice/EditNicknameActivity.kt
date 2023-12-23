59c93353fdb9cbc2eea26f680f5bd2c29851d0fc
SKM <rkdalsv@naver.com>
2023-12-24 02:08:01 +0900

    닉네임 변경 +입력후 완료 데이터 돌아가기 반영은 아직 x

--- app/src/main/java/com/example/practice/EditNicknameActivity.kt
+++ app/src/main/java/com/example/practice/EditNicknameActivity.kt
@@ -1,5 +1,7 @@
 package com.example.practice
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.practice.databinding.ActivityEditNicknameBinding
@@ -11,6 +13,18 @@ class EditNicknameActivity : AppCompatActivity() {
         binding = ActivityEditNicknameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.okBtn
+        binding.okBtn.setOnClickListener {
+//            입력한 새 닉네임 뭔지
+            val inputNewNickname = binding.newNicknameEdt.text.toString()
+
+//            입력 닉네임 메인으로
+
+            //입력 닉네임을 담아두기 위한 용도
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname",inputNewNickname)
+            setResult(Activity.RESULT_OK,resultIntent)
+            finish()
+
+        }
     }
 }
