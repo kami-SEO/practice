8b66952b0889c95e716f017df91a09750dec76f7
SKM <rkdalsv@naver.com>
2023-12-24 01:54:30 +0900

    닉네임 입력 화면 별개 추가

---
+++ app/src/main/java/com/example/practice/EditNicknameActivity.kt
@@ -0,0 +1,14 @@
+package com.example.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.practice.databinding.ActivityEditNicknameBinding
+
+class EditNicknameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditNicknameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditNicknameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
