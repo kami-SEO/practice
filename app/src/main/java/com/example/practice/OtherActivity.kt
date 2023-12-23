e0c3f1106ca912091f1870557a1f00d99289ef58
SKM <rkdalsv@naver.com>
2023-12-24 01:27:14 +0900

    finish 현재화면 종료 아닐때는 myIntent활용

--- app/src/main/java/com/example/practice/OtherActivity.kt
+++ app/src/main/java/com/example/practice/OtherActivity.kt
@@ -1,11 +1,23 @@
 package com.example.practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.practice.databinding.ActivityOtherBinding
 
 class OtherActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOtherBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_other)
+        binding = ActivityOtherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.returnToMainBtn.setOnClickListener {
+            //main으로 돌아가기
+//            val myIntent = Intent(this,MainActivity::class.java)
+//            startActivity(myIntent) 이거는 화면을 쌓아두는거
+            //실제로 돌아가는거
+            finish()
+        }
     }
 }
